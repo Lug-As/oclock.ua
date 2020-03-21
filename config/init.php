96f8b964816a23bbf20ef6bd1cb482621a9fc67e
Artyom Sokol <skaliushartem@gmail.com>
2020-03-21 23:32:23 +0300

    Start tracking

---
+++ config/init.php
@@ -0,0 +1,32 @@
+<?php
+
+function deb($var, $title = "")
+{
+	echo "<pre>";
+	if ( trim($title) != "" ) {
+		echo ucfirst($title) . ": ";
+	}
+	print_r($var);
+	echo "</pre>";
+}
+
+define("DEBUG", 1);
+define("ROOT", dirname(__DIR__) );
+define("APP", ROOT . "/app");
+define("CORE", ROOT . "/vendor/oclock/core");
+define("LIBS", ROOT . "/vendor/oclock/libs");
+define("CASH", ROOT . "/tmp/cache");
+define("CONF", ROOT . "/config");
+define("LAYOUT", "default");
+
+$path = "http";
+if (isset($_SERVER["HTTPS"]) and $_SERVER["REQUEST_SCHEME"] == "https") {
+	$path .= "s";
+}
+$path .= "://";
+$path .= $_SERVER["HTTP_HOST"];
+
+define("PATH", $path);
+define("ADMIN", PATH . "/admin");
+
+require_once ROOT . "/vendor/autoload.php";
